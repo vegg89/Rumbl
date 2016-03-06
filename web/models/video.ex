40e98b2f0fcc693133c34b953dcce292e0f6d6ef
Vincent Gutierrez <vincentgg89@gmail.com>
2016-03-05 19:35:14 -0600

    Initial commit

---
+++ web/models/video.ex
@@ -0,0 +1,27 @@
+defmodule Rumbl.Video do
+  use Rumbl.Web, :model
+
+  schema "videos" do
+    field :url, :string
+    field :title, :string
+    field :description, :string
+    belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
+
+    timestamps
+  end
+
+  @required_fields ~w(url title description)
+  @optional_fields ~w(category_id)
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+end
